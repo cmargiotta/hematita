78aba56befac77dcfe38bf5af61852aca7a3fc2c
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-07 09:16:49 +0100

    Added nix flake and .envrc

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        gitignoreSrc = pkgs.callPackage inputs.gitignore { };
+        rust = pkgs.rust-bin.stable.latest.default.override
+          {
+            extensions = [ "rust-src" "rust-analyzer" ];
+            targets = [ "wasm32-unknown-unknown" ];
+          };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          CARGO_INSTALL_ROOT = "./.cargo";
+          MONGODB = "mongodb://localhost:27017";
+          PORT = "8080";
+          REDIS = "127.0.0.1";
+          RUST_LOG = "debug";
+
+          buildInputs = with pkgs; [
+            git
+            openssl
+            cargo-generate
+            docker-compose
+            nodejs_20
+          ] ++ [ rust ];
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+
+          shellHook = ''
+            export PATH=$PATH:$PWD/.cargo/bin
+
+            #cargo install dioxus-cli
+            cargo install cargo-watch
+          '';
+        };
+      });
+}
